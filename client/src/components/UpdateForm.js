da766d13495d2faa9f899b9b56de3afaea3398da
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-07-21 13:37:05 -0400

    lecture work-in-progress

--- client/src/components/UpdateForm.js
+++ client/src/components/UpdateForm.js
@@ -15,11 +15,11 @@ const UpdateForm = props => {
 
   //3. Get the item info and add to the form
   // const id = props.match.params.id;
-  const params = useParams();
-  const history = useHistory();
+  const { id } = useParams();
+  const { push } = useHistory();
 
   useEffect(()=>{
-    axios.get(`http://localhost:3333/items/${params.id}`)
+    axios.get(`http://localhost:3333/items/${id}`)
     .then(res=>{
       setItem(res.data);
     })
@@ -45,12 +45,12 @@ const UpdateForm = props => {
   const handleSubmit = e => {
     e.preventDefault();
     //4. Click the update button
-    axios.put(`http://localhost:3333/items/${params.id}`, item)
+    axios.put(`http://localhost:3333/items/${id}`, item)
       .then(res=> {
         props.setItems(res.data);
         
         //5. Redirect to the item page
-        history.push(`/item-list/${params.id}`);
+        push(`/item-list/${id}`);
       })
       .catch(err=> {
         console.log(err);
