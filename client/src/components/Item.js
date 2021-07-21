93d7f46da94b284e78f3a79217a9447881572b19
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-07-21 13:05:32 -0400

    lecture work-in-progress

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -24,7 +24,7 @@ function Item(props) {
     //To Make An Edit:
     //1. Capture click of the edit button
     //2. Redirect to our edit form
-    props.history.push('/item-update/3');
+    props.history.push(`/item-update/${id}`);
   }
 
   return (
